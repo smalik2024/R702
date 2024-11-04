9bc1424b14097b0b2c9e74bde0b1ff40fc6d5be2
smalik2024 <smalik21@umd.edu>
2024-11-04 12:27:42 -0500

    Additional code added to the Debugging document provided in class that describes how to go about making a function and debugging it.

--- Debugging.R
+++ Debugging.R
@@ -6,15 +6,44 @@ message("Hello there")
 # Debuugging
 Dates = as.Date(c("2011-04-05", 35))
 min(Dates)
-
+mod = lm(1:2 ~ Dates)
+summary(mod)
 
 #Warning
 tmp <- log(-pi)
 is.na(tmp)
 
-# Error
-lm(x~y) #we do not have x and y in our environment
+B = 500 # boot replications
+1/B
+0.05
 
+log(pi)
+
+# Error
+lm(x ~ y, weights = z, iris, col ="blue") #we do not have x and y in our environment
+
+tapply(iris$Sepal.Length, 
+       iris$Species, 
+       quantile, 
+       probs = c(0.5),
+       na.rm = TRUE)
+
+# Functions ---- 
+lm
+
+sys.date()
+
+# Function to report Year, Month, and Day of today
+source("./code/fun_todaydate.R")
+
+
+TD()
+TDx()
+TDx(as.Date("2020-12-31"))
+TDx(as.Date(c("2020-12-31", "2028-10-16")))
+
+TDx(seq.Date(from = as.Date("2020-12-31"), 
+             to = as.Date("2021-12-31"), by = 5))
 
 #### TOOLS
 traceback()
